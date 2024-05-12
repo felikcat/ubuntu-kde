5bb0522ed2a0b87737bbe3d3281514cfc502f489
felikcat <29991266+felikcat@users.noreply.github.com>
2024-05-11 23:08:01 -0700

    Update README

--- KDE.sh
+++ KDE.sh
@@ -23,36 +23,11 @@ _config_sddm()
 
 apt update
 
+# Comment out _pkgs_remove if this is undesirable!
 PKGS=(kubuntu-* ubuntu-deskto* plasma-desktop kde-full kde-standard kde-baseapps)
 _pkgs_remove
 apt autoremove -y
 
-#
-# sddm, sddm-theme-breeze: KDE's default login manager and the default theme for it.
-# plasma-desktop: KDE itself, the minimalist version for Ubuntu.
-# plasma-workspace-wayland: Wayland support.
-# libdecor-0-0, qtwayland5, qt6-wayland: Run more programs in Wayland instead of Xorg.
-# kde-spectacle: Screenshot Utility.
-# ufw: A firewall suitable for hosting purposes.
-# konsole: Terminal Emulator.
-# xdg-desktop-portal-gnome: Required to launch some Flatpaks, such as Telegram Desktop.
-# gnome-keyring, gnome-keyring-pkcs11, libnotify-bin: Firefox (non Flatpak version) requires this for keyring support.
-#
-# dolphin: File browser.
-# -> ark: File archive support, such as Zip and 7z.
-# -> ffmpegthumbs: Video thumbnail support.
-# -> kimageformat-plugins, qt5-image-formats-plugins: Support for various image formats; https://api.kde.org/frameworks/kimageformats/html/index.html
-# -> libjxl0.7: JPEG XL support.
-# -> libpackagekitqt5-1: Required for "Configure > Configure Dolphin > Context Menu > Download New Services".
-# -> libtag1v5: Audio metadata support.
-# -> meld: "Compare files" support.
-# -> unrar: Unarchiving .rar file support.
-#
-# noto-fonts-*: The best supported fonts for making sure characters don't display as blank boxes.
-# gnome-logs: To better your ability to tell what's going on with your Linux PC.
-# systemd-oomd: Better Out Of Memory killing behavior.
-# gamemode: Automatic system tuning to maximum performance when used to run a process or game.
-#
 PKGS=(sddm sddm-theme-breeze libdecor-0-0 qtwayland5 qt6-wayland plasma-desktop kde-spectacle ufw konsole
 xdg-desktop-portal-gnome gnome-keyring gnome-keyring-pkcs11 libnotify-bin plasma-workspace-wayland
 dolphin kconfig-frontends kde-cli-tools kdegraphics-thumbnailers kimageformat-plugins
@@ -67,3 +42,5 @@ _config_sddm
 
 printf "\nSelect 'bgrt.plymouth' to restore the default Ubuntu bootup logo.\n"
 update-alternatives --config default.plymouth
+
+printf "\nKDE.sh finished, it is recommended to reboot.\n"
